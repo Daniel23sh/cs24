c5cd0bf77c3813ace1d403388ef0571462bfbc60
Daniel Shatzov <shatzovdani@gmail.com>
2025-04-16 11:22:52 +0300

    Added the scrollup method + route to ProfilePage

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import AuthCallback from './components/AuthCallback';
+import ProfilePage from './components/profilepage';
+import ScrollToTop from './components/ScrollToTop'; 
 
 const AppRoutes = () => {
   return (
     <Router>
+      <ScrollToTop /> {/* Always scroll to top on navigation */}
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/auth/callback" element={<AuthCallback />} />
+        <Route path="/tutors/:tutorName" element={<ProfilePage />} />
       </Routes>
     </Router>
   );
