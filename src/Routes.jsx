ad44b5376fdeffa21b510f55d5898166bf88c7b4
Daniel Shatzov <shatzovdani@gmail.com>
2025-04-16 11:59:34 +0300

    fixed name

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import AuthCallback from './components/AuthCallback';
-import ProfilePage from './components/profilepage';
+import ProfilePage from './components/ProfilePage';
 import ScrollToTop from './components/ScrollToTop'; 
 
 const AppRoutes = () => {
