250f2172b1b3ffdd02b43da26073ed01a155ac55
Daniel Shatzov <shatzovdani@gmail.com>
2025-05-11 17:46:56 +0300

    new

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import { BrowserRouter, HashRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import AuthCallback from './components/AuthCallback';
+import Privacy from './pages/Privacy';
+import Terms from './pages/Terms';
+import GpaCalc from './pages/GpaCalc';
+import About from './pages/About';
 
 const AppRoutes = () => {
   return (
     <Router>
-      <ScrollToTop /> {/* Always scroll to top on navigation */}
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/auth/callback" element={<AuthCallback />} />
+        <Route path="/gpa" element={<GpaCalc />} />
+        <Route path="/privacy" element={<Privacy />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </Router>
   );
